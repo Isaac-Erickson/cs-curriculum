82d29608cffc92c7410e815cf7bfe33fa512283d
Isaac-Erickson <144268763+Isaac-Erickson@users.noreply.github.com>
2024-02-27 10:50:04 -0800

    Feb. 27, 2024
    Platforms, spike, boulders

---
+++ Assets/Scripts/InstaDeath.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstaDeath : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("InstaDeath"))
+        {
+            SceneManager.LoadScene("RealPlatform");
+        }
+    }
+
+}
